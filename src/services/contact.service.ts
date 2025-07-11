b47290db1870012eca971ce78688e2acc505434e
dbhagya200 <dilinibhagya53@gmail.com>
2025-07-11 14:05:37 +0530

    contact saved

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -1,15 +1,14 @@
 import {Contact} from "../model/contact.model";
 import {contactList} from "../db/db";
-import {Product} from "../model/product.model";
 
 
 export const saveContact = (contact:Contact):Contact => {
     contactList.push(contact);
     return contact;
 }
-export const validateContact = (product:Product) => {
-    if (!product.id || !product.name || !product.price || !product.currency || !product.image) {
-        return "All fields are required";
-    }
-    return null;
+export const validateContact = (contact:Contact) => {
+  if (!contact.name || !contact.email || !contact.message) {
+      return "All fields are required";
+  }
+  return null;
 }
